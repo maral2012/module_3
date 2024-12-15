91eece9866d3cf3f0310d8531c97dbbd4a58b25a
Maral <martinovalexei568@mail.com>
2024-12-15 16:13:59 +0300

    ne gotovo

--- module_3_1.py
+++ module_3_1.py
@@ -1,5 +1,30 @@
 calls = 0
+
 def count_calls():
     global calls
-    calls+=1
-def string_info(string):
+    calls += 1
+
+def string_info(String):
+    Stroka = str(String)
+    result = (len(Stroka), Stroka.lower(), Stroka.upper())
+    count_calls()
+    return result
+
+def is_contains(string, list_to_search):
+    string = str(string).lower()
+    list_to_search = list(list_to_search)
+    count_calls()
+    for i in range(len(list_to_search)):
+        if str(list_to_search[i].lower()) == string:
+            result = True
+            break
+        else:
+            result = False
+            continue
+    return result
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
