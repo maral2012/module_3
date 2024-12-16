ad4ea5d89ad78d856435881064470192a770410c
Maral <martinovalexei568@mail.com>
2024-12-16 10:29:19 +0300

    ne gotovo

---
+++ mogule_3_0.py
@@ -0,0 +1,20 @@
+# def sum (a, b):
+#     c = a + b
+#     print (c)
+# a = 5
+# b = 3
+# sum (a, b)
+# a = 8
+# b = 12
+# sum (a, b)
+
+# def my_fn (a, b):
+#     a = a + 1
+#     c = a + b
+#     return c
+# res = my_fn (10, 3)
+# print (res)
+
+def my_fn ():
+    pass
+print (my_fn())
