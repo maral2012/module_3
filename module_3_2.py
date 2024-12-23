5ef0df1d528ced9964387646ae4d24458e18071e
Maral <martinovalexei568@mail.com>
2024-12-23 14:07:45 +0300

    gotovo

--- module_3_2.py
+++ module_3_2.py
@@ -1,13 +1,17 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
-    if ('@' and ".com" or ".ru" or ".net") not in recipient and ('@' and ".com" or ".ru" or ".net") not in sender:
-        print("Невозможно отправить письмио с адреса", sender, "на адрес", recipient)
+    def is_valid_email(email):
+        return("@" in email) and (email.endswith((".com", ".ru", ".net")))
+    if not is_valid_email(sender) or not is_valid_email(recipient):
+        print(f"Не возможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
+        return
     if sender == "university.help@gmail.com":
-        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
-
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! письмо отправлено с адреса {sender} на адрес {recipient}." )
+    return
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
